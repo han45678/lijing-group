1431d322a3cbf8e91350ac4adc813de72f17fe81
han <han0910812144@gmail.com>
2025-05-11 23:26:44 +0800

    s5

--- src/section/s5.vue
+++ src/section/s5.vue
@@ -7,14 +7,41 @@ import Fullview from '../components/fullview2.vue';
     <div class="wrapper">
       <div class="title">
         <div class="title_pic" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
-          <img class="hidden md:block" src="./s4/title.svg" alt="title">
-          <img class="block md:hidden" src="./s4/title_m.svg" alt="title">
+          <img class="hidden md:block" src="./s5/title.svg" alt="title">
+          <img class="block md:hidden" src="./s5/title_m.svg" alt="title">
         </div>
         <div class="title_text" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="500">
-          <div class="title_text_item"><img src="./s4/en_title.svg" alt="title"></div>
-          <div class="title_text_item hidden md:block"><img src="./s4/cn_title.svg" alt="title"></div>
+          <div class="title_text_item"><img src="./s5/en_title.svg" alt="title"></div>
+          <div class="title_text_item hidden md:block"><img src="./s5/cn_title.svg" alt="title"></div>
         </div>
-        <div class="cn_title block md:hidden"><img src="./s4/cn_title.svg" alt="title"></div>
+        <div class="cn_title block md:hidden"><img src="./s5/cn_title.svg" alt="title"></div>
+      </div>
+      <div class="logo"></div>
+      <div class="text">
+        麗晶相信“建材”是日常觸感的核心，【麗晶ONE】嚴選世界級精品建材，透過高品質設備，實現安全、舒適又高效的居住空間，本案採用WAFERLOCK華豐安鎖系統，開門更省力省時。衛浴設備嚴選TOTO、Villeroy & Boch、GROHE，觸感細緻、材質高級，兼具美觀、易清潔與耐用性，為生活添增質感。
+      </div>
+      <div class="pic">
+        <div class="item">
+          <img src="./s5/pic1.webp" alt="pic">
+          <p>建材示意圖，僅供參考</p>
+        </div>
+        <div class="item">
+          <img src="./s5/pic2.webp" alt="pic">
+          <p>建材示意圖，僅供參考</p>
+        </div>
+        <div class="item">
+          <img src="./s5/pic3.webp" alt="pic">
+          <p>建材示意圖，僅供參考</p>
+        </div>
+      </div>
+      <div class="author">
+        <img src="./s5/author.jpg" alt="author">
+        <div class="author_content">
+          <div class="awards">
+
+          </div>
+          <div class="author_text"></div>
+        </div>
       </div>
     </div>
   </article>
